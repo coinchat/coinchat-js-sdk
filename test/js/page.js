1abf701ede04d19e655ba203b2f5d440bc094ba1
dreamcog <dreamcog@gmail.com>
2018-07-02 23:52:21 +0800

    修复partner_no错误问题

--- test/js/page.js
+++ test/js/page.js
@@ -23,7 +23,7 @@ function coinchat_config() {
     var timestamp = Math.floor(new Date().getTime() / 1000);
     coinchat.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        partner_no  : '1528949462419631"', // 必填，唯一标识
+        partner_no  : '1528949462419631', // 必填，唯一标识
         timestamp: timestamp, // 必填，生成签名的时间戳
         nonce    : 'xxr', // 必填，生成签名的随机串
     });
@@ -33,7 +33,7 @@ function getLoginUserInfo() {
     console.log('coinchat_getLoginUserInfo');
     var timestamp = Math.floor(new Date().getTime() / 1000);
     coinchat.getLoginUserInfo({
-        partner_no  : '1528949462419631"', // 必填，唯一标识
+        partner_no  : '1528949462419631', // 必填，唯一标识
         timestamp: timestamp, // 必填，生成签名的时间戳
         success     : function(res) {
             console.log('this is a function success',res)
